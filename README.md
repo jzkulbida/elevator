fb94d058e24b8b1215a2928b13d5cab75d708654
Jie Kulbida <jkulbida@kmbs.konicaminolta.us>
2024-06-20 15:19:24 -0600

    Added support for kvp syntax in arguments

--- README.md
+++ README.md
@@ -5,21 +5,34 @@
 Python must be installed on the OS 
 
 ## Usage: 
-python elevator.py --start <start floor num> --floors <floors to visit>
+python elevator.py start=<start floor num> floors=<floors to visit>
 
 ## Arguments:
---start: an integer specifying the starting floor
+<start floor num>: an integer specifying the starting floor
 
---floors: a comma separated list of numbers, specifying the floors to visit
+<floors to visit>: a comma separated list of numbers, specifying the floors to visit
 
-Note: Do not add whitespaces within "--floor" argument
+Note: <floors to visit> should not contain white space, unless the entire string is surrounded by quotes
 
 ## Example:
 
-python elevator.py --start 20 --floors 4,7,-6,20
+python elevator.py start=20 floors=4,7,-6,20
+
+or
+
+python elevator.py start=20 floors="4, 7, -6, 20"
 
 ## Output:
 [Total travel time] [start floor num] [floors to visit]
 
 For the example above, output: 580 20,4,7,-6,20
 
+## Assumptions:
+
+1. Each floor must be identified with an integer value (0, positive or negative)
+2. Each floor number must be within the range of [min_floor_number, max_floor_number]
+3. For demonstration, min_floor_number and max_floor_number are constants set to -100 and 200, respectively 
+4. min_floor_number and max_floor_number should be modified to reflect the actual elevator being simulated
+5. If any of the conditions 1-4 above are not met, program will abort with error
+6. Repeating floor numbers (in succession) will raise warning, but output will be generated with repeated floors removed
+7. If no errors in arguments, and no valid floors are received, a warning will be raised, but output will be generated with: 0 [starting_floor]
--- elevator.py
+++ elevator.py
@@ -1,37 +1,120 @@
 import argparse
+import sys
 
-if __name__ == '__main__':
+
+def print_usage():
+    print(f"Usage: {sys.argv[0]} start=<starting floor> floor=<floors to visit>")
+
+
+# ==================================================================================
+# Not used - Leverage the Python argparse library (must specify arguments using --start and --floors)
+# ==================================================================================
+"""
+def get_arguments_using_argparser():
     parser = argparse.ArgumentParser(description='Elevator Simulator')
     parser.add_argument("--start", type=int, required=True, help="Starting floor number")
     parser.add_argument("--floors", required=True, help="Floors to visit (separated by comma), e.g.: 3,5,-1,0")
     args = parser.parse_args()
 
     # get the starting floor number
-    start_floor = args.start
+    start = args.start
 
     # parse the "floors" argument and split it into one or more floors
-    floors_string = args.floors
+    floors = args.floors
+    return start, floors
+"""
+
+
+# ==================================================================================
+# Extract attribute-value pairs of interest (start, floors) from command-line arguments
+# ==================================================================================
+
+def get_arguments_using_avps():
+    start = 0
+    floors = ""
+
+    error_occurred = False
+    start_keyword = "start"
+    floors_keyword = "floor"
+    if len(sys.argv) < 3:
+        print(f"Expecting 2 arguments, but got {len(sys.argv) - 1}")
+    else:
+        for arg in sys.argv[1:]:
+            # arg should be an attribute/value pair separated by "="
+            fields = arg.split('=')
+            if len(fields) == 2:
+                key = fields[0]
+                value = fields[1]
+                if key == start_keyword:
+                    try:
+                        start = int(value)
+                    except ValueError:
+                        print(f'*** Error: Value for starting floor is not an integer: {arg}')
+                        error_occurred = True
+                        break
+                elif key == floors_keyword:
+                    floors = value
+                else:
+                    print(f"Unknown key found in argument: {arg} - ignored")
+            else:
+                print(f'***Warning: each argument should contain one "=". Ignoring "{arg}"')
+                error_occurred = True
+                break
+
+    return error_occurred, start, floors
+
+
+# ===================================================
+# Elevator Simulator Entry point
+# ===================================================
+
+if __name__ == '__main__':
+
+    arg_error, start_floor, floors_string_orig = get_arguments_using_avps()
+
+    if arg_error:
+        print(f"*** Error in parsing arguments")
+        print_usage()
+        exit(-1)
+
+    """
+    As an alternative, here is an implementation using argparser package
+    start_floor, floors_string = get_arguments_using_argparser()
+    """
+
+    # extract and validate floor numbers in floors_string
     floors_to_visit = []
     # replace ',' with space, so that we can tolerate leading, trailing, or repeating commas
-    floors_string = floors_string.replace(",", " ")
+    floors_string = floors_string_orig.replace(",", " ")
+
+    # iterate the split fields, and append floor number to floors_to_visit
     splits = floors_string.split()
     parse_ok = True
+    max_floor_number = 200
+    min_floor_number = -100
     for i, f in enumerate(splits):
         try:
             floor_int = int(f)
-            floors_to_visit.append(floor_int)
+            if min_floor_number <= floor_int <= max_floor_number:
+                floors_to_visit.append(floor_int)
+            else:
+                print(f"Error: floor number must be within the range: [{min_floor_number}, {max_floor_number}]: {f}")
+                parse_ok = False
+                break
         except ValueError:
-            print(f"*** Found invalid floor number in field #{i+1}: {f}")
-            print(f"*** Please use comma separated integers")
+            print(f"*** Error: Found invalid floor number in field #{i + 1}, found {f}")
             parse_ok = False
             break
 
+    # handle exceptions
     if not parse_ok:
-        print (f"*** Error occurred in parsing 'floors' string: {args.floors}")
+        print(f"*** Error occurred in parsing 'floors' string: {floors_string_orig}")
+        print_usage()
         exit(-1)
 
     if len(floors_to_visit) == 0:
         print("Warning: no floors to visit!")
+        print_usage()
 
     # construct the output string and compute total time
     travel_time_per_floor = 10
@@ -48,4 +131,3 @@ if __name__ == '__main__':
 
     # output results
     print(f"{total_time} {start_floor}{output_floors}")
-
